e519a56d942c5a2e39a9e87b4c5e7ea292fbba19
Макаров Александр <alebarda2018@gmail.com>
2025-08-19 20:54:49 +0300

    Изменил config. Создал движок для подключения к базе в development.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,13 +50,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 LANGUAGE_CODE = "ru-ru"
 USE_TZ = True
 
