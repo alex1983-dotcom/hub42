2f07ff75d3c556bbd46d346fa68a2913483c80f8
Макаров Александр <alebarda2018@gmail.com>
2025-10-24 22:09:08 +0300

    Настройка блога

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,6 +1,8 @@
-from django.db import models
-from django.utils import timezone
 from django.utils.text import slugify
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from django.urls import reverse
 from mdeditor.fields import MDTextField
 from apps.core.models import TimeStampedModel
 
@@ -64,11 +66,14 @@ class Post(TimeStampedModel):
     def __str__(self):
         return self.title
 
+    def clean(self):
+        if self.published_at and self.published_at > timezone.now():
+            raise ValidationError("Дата публикации не может быть в будущем.")
+
     def save(self, *args, **kwargs):
         if self.status == "published" and not self.published_at:
             self.published_at = timezone.now()
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        from django.urls import reverse
         return reverse("blog:detail", kwargs={"slug": self.slug})
