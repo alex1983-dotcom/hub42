250792ea6e8219b02a0de364da1f917128a71203
Макаров Александр <alebarda2018@gmail.com>
2025-11-15 13:18:39 +0300

    Контент: создание фикстур для фронтенда. Сделаны системные блоки в админке, приложения social

--- apps/social/admin.py
+++ apps/social/admin.py
@@ -4,8 +4,18 @@ from .models import SocialLink
 
 @admin.register(SocialLink)
 class SocialLinkAdmin(admin.ModelAdmin):
-    list_display = ("name", "url", "is_active")
+    change_form_template = 'admin/social/change_form.html'
+    list_display = ("name", "url", "is_active", "is_system")
     list_editable = ("is_active",)
 
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.is_system:
+            return False
+        return super().has_delete_permission(request, obj)
 
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
 
--- apps/social/models.py
+++ apps/social/models.py
@@ -5,6 +5,7 @@ class SocialLink(models.Model):
     name = models.CharField(max_length=20, unique=True)
     url = models.URLField()
     is_active = models.BooleanField(default=True)
+    is_system = models.BooleanField("Системная ссылка", default=False, editable=False)
 
     def __str__(self):
         return self.name
