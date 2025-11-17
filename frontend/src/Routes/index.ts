f303bfad14e26d6f6213080096be8264716d71ec
Julian Mosievskiy <yulik.masievskiy@mail.ru>
2025-11-17 20:11:22 +0300

    some_stuff

--- frontend/src/Routes/index.ts
+++ frontend/src/Routes/index.ts
@@ -2,7 +2,8 @@ export const routes = {
    main: "/",
    printer: "/printer/:id",
    blog: "/blog",
-   opinions:"/opinions",
-   blogId: (id: string | number) => `/blog/${id}`,
+   opinions: "/opinions",
+   blogArticle: "/blog/:slug",
    printerId: (id: string | number) => `/printer/${id}`,
+   blogId: (slug: string) => `/blog/${slug}`,
 } as const;
